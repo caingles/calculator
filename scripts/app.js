f0ffd4d334cc5d090674c3a448a344e047c311a0
Christian Ingles <christianarguetaaa@gmail.com>
2023-01-26 21:39:22 -0600

    add function to buttons and display on screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,43 @@
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+
+const calculate = (n1, operator, n2) => {
+  let result = "";
+
+  if (operator === "add") {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === "subtract") {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === "multiply") {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === "divide") {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+
+  return result;
+};
 
 keys.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
     // Do something
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
+
+    const previousKeyType = calculator.dataset.previousKeyType;
 
     if (!action) {
-      console.log("number key!");
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
     }
 
     if (
@@ -17,11 +46,15 @@ keys.addEventListener("click", (e) => {
       action === "multiply" ||
       action === "divide"
     ) {
-      console.log("operator key!");
+      key.classList.add("is-depressed");
+      // Add custom attribute
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
 
     if (action === "decimal") {
-      console.log("decimal key!");
+      display.textContent = displayedNum + ".";
     }
 
     if (action === "clear") {
@@ -29,7 +62,11 @@ keys.addEventListener("click", (e) => {
     }
 
     if (action === "calculate") {
-      console.log("equal key!");
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
   }
 });
